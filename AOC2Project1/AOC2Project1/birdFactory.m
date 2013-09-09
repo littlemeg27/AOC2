7b91b38250e688ddb98d22e2c3759bbc3d3ee6d2
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-09-08 22:26:38 -0400

    Last few changes, still errors

--- AOC2Project1/AOC2Project1/birdFactory.m
+++ AOC2Project1/AOC2Project1/birdFactory.m
@@ -12,16 +12,20 @@
 
 +(birdBase *)callBirds:(int)birdType
 {
-    switch (birdType) {
+    switch (birdType)
+    {
         case AMAZON:
             return [[amazonBird alloc] init];
             break;
+            
         case ECLECTUS:
             return [[eclectusBird alloc] init];
             break;
+            
         case GREENCHEEK:
             return [[greenCheekBird alloc] init];
             break;
+            
         default:
             return nil;
             break;
