31d39d6e964c4f27972ed6aa84892b618653c990
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-09-19 02:54:41 -0400

    First commit almost no work done

---
+++ AOC2Project3/AOC2Project3/AOC2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  AOC2ViewController.h
+//  AOC2Project3
+//
+//  Created by Brenna Pavlinchak on 9/18/13.
+//  Copyright (c) 2013 Brenna Pavlinchak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AOC2ViewController : UIViewController
+
+@end
---
+++ AOC2Project3/AOC2Project3/AOC2ViewController.m
@@ -0,0 +1,29 @@
+//
+//  AOC2ViewController.m
+//  AOC2Project3
+//
+//  Created by Brenna Pavlinchak on 9/18/13.
+//  Copyright (c) 2013 Brenna Pavlinchak. All rights reserved.
+//
+
+#import "AOC2ViewController.h"
+
+@interface AOC2ViewController ()
+
+@end
+
+@implementation AOC2ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
