e706e0994ba78b5edd74b3497d426200e00409a2
Brenna Pavlinchak <littlemeg27@fullsail.edu>
2013-09-21 17:59:39 -0400

    Project deleted a third time, trying again

--- AOC2Project3/AOC2Project3/main.m
+++ AOC2Project3/AOC2Project3/main.m
@@ -2,11 +2,7 @@
 //  main.m
 //  AOC2Project3
 //
-<<<<<<< HEAD
 //  Created by Brenna Pavlinchak on 9/21/13.
-=======
-//  Created by Brenna Pavlinchak on 9/18/13.
->>>>>>> 1da458dd409250bc941a38e4d4685a586c6554c6
 //  Copyright (c) 2013 Brenna Pavlinchak. All rights reserved.
 //
 
--- AOC2Project3/AOC2Project3Tests/AOC2Project3Tests.h
+++ AOC2Project3/AOC2Project3Tests/AOC2Project3Tests.h
@@ -2,11 +2,7 @@
 //  AOC2Project3Tests.h
 //  AOC2Project3Tests
 //
-<<<<<<< HEAD
 //  Created by Brenna Pavlinchak on 9/21/13.
-=======
-//  Created by Brenna Pavlinchak on 9/18/13.
->>>>>>> 1da458dd409250bc941a38e4d4685a586c6554c6
 //  Copyright (c) 2013 Brenna Pavlinchak. All rights reserved.
 //
 
